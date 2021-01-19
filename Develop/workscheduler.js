fe4f2d0c7a8f39a4509951cdf0affb3b3bf44e2a
CGC <70499508+cgconverse@users.noreply.github.com>
2021-01-18 19:04:58 -0800

    wrote if else statement to determine am or pm

--- Develop/workscheduler.js
+++ Develop/workscheduler.js
@@ -10,14 +10,28 @@ var hoursOfDay = ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
 var displayContent = $("#displayContent");
 
 for (var i = 0; i < hoursOfDay.length; i++) {
-
+// Creating a new row 
     var createRow = $("<div>");
+//giving it a class of "row"
     createRow.addClass("row");
     createRow.data("hour", hoursOfDay[i])
-
+// Creating a new column for the hour
     var hourCol = $("<div>");
+// adding class to make it mobile reponsive
     hourCol.addClass("col-md-2 col-sm-3 time-block");
-    
+
+// Creating an if/else statement to determine if it's AM or PM
+    if(hoursOfDay[i] === 12) {
+        hourCol.text("12pm") 
+    } else if (hoursOfDay[i] > 12) {
+        hourCol.text(parseInt(hoursOfDay[i] - 12) + "pm");
+    } else {
+        hourCol.text(parseInt(hoursOfDay[i]) + "am")
+    }
+
+// Creating a column for the tasks
+    var taskColumn = $("<input>");
+    console.log()
 }
 
 
